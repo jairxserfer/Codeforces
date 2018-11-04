acb069671922b2d1001c28c903fb3af354ea07d9
Harry Bazalar Lopez <bazalar.harry@ibm.com>
2018-11-03 20:57:10 -0500

    451A

---
+++ 577A.cpp
@@ -0,0 +1,169 @@
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+//#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+#define  ll long long
+#define MAX 100001
+
+using  namespace std;
+vector<bool> is_prime(10000001);
+void sieve_atkins(ll int n)
+{
+
+    is_prime[2] = true;
+    is_prime[3] = true;
+    for (ll int i = 5; i <= n; i++)
+    {
+        is_prime[i] = false;
+    }
+    ll int lim = ceil(sqrt(n));
+    for (ll int x = 1; x <= lim; x++)
+    {
+        for (ll int  y = 1; y <= lim; y++)
+        {
+            ll int num = (4 * x * x + y * y);
+            if (num <= n && (num % 12 == 1 || num % 12 == 5))
+            {
+                is_prime[num] = true;
+            }
+            num = (3 * x * x + y * y);
+            if (num <= n && (num % 12 == 7))
+            {
+                is_prime[num] = true;
+            }
+
+            if (x > y)
+            {
+                num = (3 * x * x - y * y);
+                if (num <= n && (num % 12 == 11))
+                {
+                    is_prime[num] = true;
+                }
+            }
+        }
+    }
+    for (ll int i = 5; i <= lim; i++)
+    {
+        if (is_prime[i])
+        {
+            for (ll int  j = i * i; j <= n; j += i)
+            {
+                is_prime[j] = false;
+            }
+        }
+    }
+
+
+}
+
+ll int solution(ll int size, ll int number){
+  ll  int cont = 0;
+  if(number == 1 && size == 1)
+    return 1;
+  if( number > size && size == 1)
+      return 0;
+  if( number > size && is_prime[number]  )
+    return 0;
+  for(int i = 1 ; i < size; i++)
+  {
+    if(number % i == 0)
+    {
+      if(i == 1 && number != 1) cont = 0;
+      cont++;
+    }
+  }
+  return cont;
+}
+
+
+int main(){
+
+  ll  size, number;
+  ll aleards = 10000000;
+  sieve_atkins(aleards);
+  cin>>size>>number;
+  cout<<solution(size,number)<<endl;
+  return 0;
+}
